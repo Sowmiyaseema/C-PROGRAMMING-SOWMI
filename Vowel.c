142b6c3deeec018cf46c523c07f8f906cb4f9765
Sowmiyaseema <156098226+Sowmiyaseema@users.noreply.github.com>
2026-02-12 09:46:52 +0530

    Create Vowel.c

---
+++ Vowel.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+   
+    printf("Enter any character: ");  /* Input character from user */
+    scanf("%c", &ch);
+
+    /* Condition for vowel */
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || 
+       ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+    {
+        printf("'%c' is Vowel.", ch);
+    }
+    else if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        /* Condition for consonant */
+        printf("'%c' is Consonant.", ch);
+    }
+    else 
+    {
+        /*
+         * If it is neither vowel nor consonant
+         * then it is not an alphabet.
+         */
+        printf("'%c' is not an alphabet.", ch);
+    }
+
+    return 0;
+}
